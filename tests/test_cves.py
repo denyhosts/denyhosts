d92de1f8601cb8cd168714440283149214af4548
Matt Ruffalo <matt.ruffalo@gmail.com>
2014-12-09 23:09:03 -0500

    Add failing test cases for CVE-2006-6301 and CVE-2007-4323
    This should serve as good motivation to add test coverage for these.

---
+++ tests/test_cves.py
@@ -0,0 +1,10 @@
+from __future__ import print_function, unicode_literals
+
+import unittest
+
+class CVEsTestCase(unittest.TestCase):
+    def test_cve_2006_6301(self):
+        self.assertTrue(False)
+
+    def test_cve_2007_4323(self):
+        self.assertTrue(False)
