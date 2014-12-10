076a17e1423b01399273d04dae9882807ebd6a75
Matt Ruffalo <matt.ruffalo@gmail.com>
2014-12-10 09:52:15 -0500

    Add empty, failing test case for CVE-2013-6890 (and test data!)

--- tests/test_cves.py
+++ tests/test_cves.py
@@ -2,9 +2,19 @@ from __future__ import print_function, unicode_literals
 
 import unittest
 
+# From Helmut Grohne's disclosure of this CVE
+CVE_2013_6890_LINES = """
+sshd[123]: Invalid user Invalid user root from 123.123.123.123 from 21.21.21.21
+sshd[123]: input_userauth_request: invalid user Invalid user root from 123.123.123.123 [preauth]
+sshd[123]: Connection closed by 21.21.21.21 [preauth]
+"""
+
 class CVEsTestCase(unittest.TestCase):
     def test_cve_2006_6301(self):
         self.assertTrue(False)
 
     def test_cve_2007_4323(self):
+        self.assertTrue(False)
+
+    def test_cve_2013_6890(self):
         self.assertTrue(False)
