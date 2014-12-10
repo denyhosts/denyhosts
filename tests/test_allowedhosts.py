7562c1c8af2883e71be210d7a852cc080f6ba2b8
Matt Ruffalo <matt.ruffalo@gmail.com>
2014-12-09 22:03:31 -0500

    test_allowedhosts.py: fix for Python 2.6

--- tests/test_allowedhosts.py
+++ tests/test_allowedhosts.py
@@ -8,9 +8,8 @@ from DenyHosts.allowedhosts import AllowedHosts
 class AllowedHostsBase(unittest.TestCase):
     def setUp(self):
         data_dir = ospj(dirname(abspath(__file__)), 'data')
-        allowed_hosts_filename = ospj(data_dir, 'allowedhosts.txt')
-        # Initialize minimal preferences dict: just enough for the
-        # AllowedHosts constructor
+        # Initialize minimal preferences dict: just enough
+        # for the AllowedHosts constructor
         prefs = {
             'WORK_DIR': data_dir,
             'ALLOWED_HOSTS_HOSTNAME_LOOKUP': 'true',
@@ -18,12 +17,13 @@ class AllowedHostsBase(unittest.TestCase):
         self.allowed_hosts = AllowedHosts(prefs)
         self.allowed_hosts.load_hosts()
 
+# TODO replace with assertIn/assertNotIn when dropping support for 2.6
 class AllowedHostsBasicTest(AllowedHostsBase):
     def test_positive(self):
-        self.assertIn('127.0.0.1', self.allowed_hosts)
+        self.assertTrue('127.0.0.1' in self.allowed_hosts)
 
     def test_negatives(self):
-        self.assertNotIn('10.0.0.1', self.allowed_hosts)
-        self.assertNotIn('127.0.0.2', self.allowed_hosts)
-        self.assertNotIn('0.0.0.0', self.allowed_hosts)
-        self.assertNotIn('bogus', self.allowed_hosts)
+        self.assertFalse('10.0.0.1' in self.allowed_hosts)
+        self.assertFalse('127.0.0.2' in self.allowed_hosts)
+        self.assertFalse('0.0.0.0' in self.allowed_hosts)
+        self.assertFalse('bogus' in self.allowed_hosts)
