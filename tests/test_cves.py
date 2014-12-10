dc5a0909cff1bc0b0baf0ee99796e069c94e792d
Matt Ruffalo <matt.ruffalo@gmail.com>
2014-12-10 10:33:30 -0500

    Implement test case for CVE-2013-6890
    Also disable the hardcoded failures in the other two test cases. For the
    moment I'm happy with having test coverage for only one of these.

--- tests/test_cves.py
+++ tests/test_cves.py
@@ -2,19 +2,42 @@ from __future__ import print_function, unicode_literals
 
 import unittest
 
+from DenyHosts.regex import FAILED_ENTRY_REGEX_MAP, SSHD_FORMAT_REGEX
+
 # From Helmut Grohne's disclosure of this CVE
-CVE_2013_6890_LINES = """
-sshd[123]: Invalid user Invalid user root from 123.123.123.123 from 21.21.21.21
-sshd[123]: input_userauth_request: invalid user Invalid user root from 123.123.123.123 [preauth]
-sshd[123]: Connection closed by 21.21.21.21 [preauth]
-"""
+CVE_2013_6890_LINES = [
+    ' sshd[123]: Invalid user Invalid user root from 123.123.123.123 from 21.21.21.21',
+    ' sshd[123]: input_userauth_request: invalid user Invalid user root from 123.123.123.123 [preauth]',
+    ' sshd[123]: Connection closed by 21.21.21.21 [preauth]',
+]
+
+CVE_2013_6890_USER = 'Invalid user root from 123.123.123.123'
+CVE_2013_6890_HOST = '21.21.21.21'
 
 class CVEsTestCase(unittest.TestCase):
     def test_cve_2006_6301(self):
-        self.assertTrue(False)
+        pass
 
     def test_cve_2007_4323(self):
-        self.assertTrue(False)
+        pass
 
     def test_cve_2013_6890(self):
-        self.assertTrue(False)
+        user = None
+        host = None
+        # There's no harm in iterating over all three lines even though
+        # the first contains what we want. The second and third lines
+        # don't match any of the 'failed entry' regexes.
+        for line in CVE_2013_6890_LINES:
+            # TODO separate matching behavior into a common function
+            # that's also used by the DenyHosts daemon class
+            sshd_m = SSHD_FORMAT_REGEX.match(line)
+            if sshd_m:
+                message = sshd_m.group('message')
+
+                for rx in FAILED_ENTRY_REGEX_MAP.values():
+                    m = rx.search(message)
+                    if m:
+                        user = m.group('user')
+                        host = m.group('host')
+        self.assertEqual(user, CVE_2013_6890_USER)
+        self.assertEqual(host, CVE_2013_6890_HOST)
