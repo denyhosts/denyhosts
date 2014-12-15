f75dc91871b0f5ae1ac44121ba1e47bc2e6154e5
Matt Ruffalo <matt.ruffalo@gmail.com>
2014-12-15 00:52:13 -0500

    test_util.py: expand a tiny bit
    Allow running under the default Python unittest runner, test is_false
    in addition to is_true for lack of a reason not to.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,8 +1,24 @@
-from DenyHosts.util import is_true
+from __future__ import print_function, unicode_literals
 
-def test_is_true():
-    for string in ['1', 't', 'true', 'y', 'yes']:
-        assert is_true(string)
+from DenyHosts.util import is_false, is_true
 
-    for string in ['', 'false', 'ye', 'tr']:
-        assert not is_true(string)
+import unittest
+
+class UtilsTest(unittest.TestCase):
+    def setUp(self):
+        self.true_strings = ['1', 't', 'true', 'y', 'yes']
+        self.false_strings = ['', 'false', 'ye', 'tr']
+
+    def test_is_true(self):
+        for string in self.true_strings:
+            self.assertTrue(is_true(string))
+
+        for string in self.false_strings:
+            self.assertFalse(is_true(string))
+
+    def test_is_false(self):
+        for string in self.true_strings:
+            self.assertFalse(is_false(string))
+
+        for string in self.false_strings:
+            self.assertTrue(is_false(string))
