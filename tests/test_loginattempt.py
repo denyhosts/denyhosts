862bf7442c0ac29a897ec792329773cd5f891bbb
Matt Ruffalo <matt.ruffalo@gmail.com>
2014-12-23 15:51:34 -0500

    Refactor LoginAttempt: prepare to write to data files

--- tests/test_loginattempt.py
+++ tests/test_loginattempt.py
@@ -6,6 +6,30 @@ import unittest
 
 from DenyHosts.prefs import Prefs
 from DenyHosts.loginattempt import LoginAttempt
+from DenyHosts.constants import (
+    ABUSIVE_HOSTS_INVALID,
+    ABUSIVE_HOSTS_ROOT,
+    ABUSIVE_HOSTS_RESTRICTED,
+    ABUSIVE_HOSTS_VALID,
+    ABUSED_USERS_INVALID,
+    ABUSED_USERS_VALID,
+    ABUSED_USERS_AND_HOSTS,
+    SUSPICIOUS_LOGINS,
+)
+
+# List of 2-tuples:
+# [0] filename in which data is stored
+# [1] LoginAttempt attribute name that reads from this file
+LOGIN_ATTEMPT_ATTRIBUTE_FILENAMES = [
+    (ABUSIVE_HOSTS_INVALID, 'get_abusive_hosts_invalid'),
+    (ABUSIVE_HOSTS_ROOT, 'get_abusive_hosts_root'),
+    (ABUSIVE_HOSTS_RESTRICTED, 'get_abusive_hosts_restricted'),
+    (ABUSIVE_HOSTS_VALID, 'get_abusive_hosts_valid'),
+    (ABUSED_USERS_INVALID, 'get_abused_users_invalid'),
+    (ABUSED_USERS_VALID, 'get_abused_users_valid'),
+    (ABUSED_USERS_AND_HOSTS, 'get_abused_users_and_hosts'),
+    (SUSPICIOUS_LOGINS, 'get_suspicious_logins'),
+]
 
 class BasicLoginAttemptTest(unittest.TestCase):
     def setUp(self):
@@ -24,11 +48,5 @@ class BasicLoginAttemptTest(unittest.TestCase):
 
     def test_no_data_file(self):
         login_attempt = LoginAttempt(self.prefs, set(self.allowed_hosts))
-        self.assertFalse(login_attempt.get_abusive_hosts_invalid())
-        self.assertFalse(login_attempt.get_abusive_hosts_root())
-        self.assertFalse(login_attempt.get_abusive_hosts_restricted())
-        self.assertFalse(login_attempt.get_abusive_hosts_valid())
-        self.assertFalse(login_attempt.get_abused_users_invalid())
-        self.assertFalse(login_attempt.get_abused_users_valid())
-        self.assertFalse(login_attempt.get_abused_users_and_hosts())
-        self.assertFalse(login_attempt.get_suspicious_logins())
+        for filename, method_name in LOGIN_ATTEMPT_ATTRIBUTE_FILENAMES:
+            self.assertFalse(getattr(login_attempt, method_name)())
