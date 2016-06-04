7194e865d2c13bfab7e70f862799a0a1c29d7c25
Mauro Crociara <admin@bitdepot.io>
2016-06-04 18:46:10 +0200

    Fixed the AttributeError related to issue 54. Isinstance() is compatible with python 2.7 and python 3.x

--- DenyHosts/report.py
+++ DenyHosts/report.py
@@ -1,9 +1,6 @@
 import logging
 import re
 import socket
-# Removing this as it causes runtime errors on Python3.4
-# from types import ListType, TupleType
-import types
 from .util import is_true
 try:
     import syslog
@@ -39,8 +36,8 @@ class Report:
     def add_section(self, message, iterable):
         self.report += "%s:\n\n" % message
         for i in iterable:
-            # if type(i) in (TupleType, ListType):
-            if (type(i) is types.ListType) or (type(i) is types.TupleType):
+            # checks the item type (list or tuple)
+            if isinstance(i, list) or isinstance(i, tuple):
                 extra = ": %d\n" % i[1]
                 i = i[0]
             else:
