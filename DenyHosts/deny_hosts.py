dffb6f9f08ff66a24e502c3f005f70e2e2aff86d
Jose <josev814@gmail.com>
2016-07-13 15:52:37 -0400

    adding in -m multiport in order to allow multiple ports
    Had to add in multiport into the iptable rules for multiple ports to work on line 343

--- DenyHosts/deny_hosts.py
+++ DenyHosts/deny_hosts.py
@@ -340,7 +340,7 @@ allowed based on your %s file"""  % (self.__prefs.get("HOSTS_DENY"),
               for host in new_hosts:
                   my_host = str(host)
                   if ',' in self.__blockport:
-                      new_rule = self.__iptables + " -I INPUT -p tcp --dports " + self.__blockport + " -s " + my_host + " -j DROP"
+                      new_rule = self.__iptables + " -I INPUT -p tcp -m multiport --dports " + self.__blockport + " -s " + my_host + " -j DROP"
                   elif self.__blockport:
                      new_rule = self.__iptables + " -I INPUT -p tcp --dport " + self.__blockport + " -s " + my_host + " -j DROP"
                   else:
