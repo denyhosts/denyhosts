f9a42faab57f31f4e8b0a8b47dccebab4ccc51f6
Jose <josev814@gmail.com>
2016-07-15 14:52:32 -0400

    Created sendToSlack.py plugin
    Adding a plugin that allows deny notifications to be sent to slack.

---
+++ plugins/sendToSlack.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#
+# Jose' Vargas <https://github.com/josev814>
+# This plugin allows deny host to send a blocked ip to slack as a notification
+# To enable the plugin edit /etc/denyhosts.conf
+# Uncomment PLUGIN_DENY and point it to the location of this file
+# Edit lines 16 and 18 to your slack web hook url and channel
+# Tested on Python 2.7.9
+#
+import sys
+import urllib2
+import socket
+
+# set the slack options
+#Slack Web Hook ex: https://hooks.slack.com/services/xxxxxxx/xxxxxxxxx/xxxxxxxxxxxxxxxxxxxx
+slackWebHook=''
+#channel to post in slack, include the # in front of the channel ex: #systems
+channel=''
+
+# Get Server Info
+serverName=socket.gethostbyname_ex(socket.gethostname())[0]
+externalServerIp=socket.gethostbyname_ex(socket.gethostname())[2][1]
+internalServerIp=socket.gethostbyname_ex(socket.gethostname())[2][0]
+
+message="%s ip blocked on %s (%s / %s)" % ( sys.argv[1], serverName, externalServerIp, internalServerIp )
+
+#if channel is set use the channel defined, otherwise post to the channel the webhook was made for
+if channel != "":
+        data='payload={"text":"' + message + '","channel":"' + channel + '"}'
+else:
+        data='payload={"text":"' + message + '"}'
+
+if slackWebHook != "":
+        request = urllib2.Request(slackWebHook, data)
+        call = urllib2.urlopen(req)
+        call.close()
+
+sys.exit(0)
