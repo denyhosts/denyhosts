6de1d6a422d34b401e1c2e3abe4f323fc94e1d86
Jan-Pascal van Best <janpascal@vanbest.org>
2017-07-16 22:59:11 +0200

    Added unit test for is_valid_ip_address

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,13 +2,30 @@ from __future__ import print_function, unicode_literals
 
 import unittest
 
-from DenyHosts.util import is_false, is_true, parse_host
+from DenyHosts.util import is_false, is_true, parse_host, is_valid_ip_address
 
 # List of 2-tuples: line to parse, expected host
 HOST_TEST_DATA = [
     ('host', 'host'),
     ('ALL:127.0.0.1', '127.0.0.1'),
     (3, ''),
+]
+
+VALID_IPADDRESS_DATA = [
+    '216.58.206.100',
+    '2.19.61.200',
+    '11.28.51.201',
+    '8.8.8.8'
+]
+
+INVALID_IPADDRESS_DATA = [
+    '127.0.0.1',
+    '192.168.16.2',
+    '10.10.10.10',
+    '169.254.0.1',
+    '172.16.12.51',
+    '225.22.62.1',
+    '242.125.34.43'
 ]
 
 class UtilsTest(unittest.TestCase):
@@ -33,3 +50,11 @@ class UtilsTest(unittest.TestCase):
     def test_parse_host(self):
         for line, expected in HOST_TEST_DATA:
             self.assertEqual(parse_host(line), expected)
+
+    def test_valid_ip_address(self):
+        for ip in VALID_IPADDRESS_DATA:
+            self.assertTrue(is_valid_ip_address(ip))
+
+    def test_invalid_ip_address(self):
+        for ip in INVALID_IPADDRESS_DATA:
+            self.assertFalse(is_valid_ip_address(ip))
