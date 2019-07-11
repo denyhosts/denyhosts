ef308a523669f5610c3cd1173f67c45040ede1c5
Jose Vargas <jose.vargas@tmmdata.com>
2019-07-11 12:25:00 -0400

    updating the slack plugin to use a config file using configparser to allow a config file to exist without the plugin update(s) causing issues

--- plugins/sendToSlack.py
+++ plugins/sendToSlack.py
@@ -4,50 +4,78 @@
 # This plugin allows deny host to send a blocked ip to slack as a notification
 # To enable the plugin edit /etc/denyhosts.conf
 # Uncomment PLUGIN_DENY and point it to the location of this file
-# Edit lines 16 and 18 to your slack web hook url and channel
+# create slack.conf in the plugins directory and set the slack url, webhooktoken, channel
+#   or run denyhosts with the plugin enabled, and allow the plugin to create the file on it's own
+#   then stop denyhosts and edit the create slack.conf file to match the settings you need
 # chmod +x this file so that it's executable, or the logs will show plugin returned 32256
 # Tested on Python 2.7.9
 #
+
 import sys
+import os
 import urllib2
 import socket
+import re
+from configparser import ConfigParser
 
 # Enable/Disable the option to include/exclude ips in slack messages
 enableIps=False
+SLACKTOKEN=''
+SLACKURL=''
+SLACKCHANNEL=''
 
 # set the slack options
+slackConfig = 'slack.conf'
+config = ConfigParser()
+if os.path.exists(slackConfig) is False:
+    config.add_section('default')
+    config.set('default', 'url', 'https://hooks.slack.com/services/')
+    config.set('default', 'webhooktoken', 'xxx/xxx/xxx')
+    config.set('default', 'channel', '')
+    with open(slackConfig, 'w') as configfile:
+        config.write(configfile)
+
+config.read(slackConfig)
+if 'default' in config:
+    if 'url' in config['default'] and re.match(r'([\w\:\/\.]+)', config['default']['url'], re.IGNORECASE):
+        SLACKURL = re.findall(r'([\w\:\/\.]+)', config['default']['url'], re.IGNORECASE)[0]
+    if 'webhooktoken' in config['default'] and re.match(r'([\w\d\-/]+)', config['default']['webhooktoken'], re.IGNORECASE):
+        SLACKTOKEN = re.findall(r'([\w\-]+)', config['default']['webhooktoken'], re.IGNORECASE)[0]
+
+    # channel to post in slack, include the # in front of the channel ex: #systems
+    if 'channel' in config['default'] and re.match(r'(^(@|#)[\w\d\-_/.]+)', config['default']['channel'],re.IGNORECASE):
+        SLACKCHANNEL = re.findall(r'([\w\-]+)', config['default']['webhooktoken'], re.IGNORECASE)[0]
+
 #Slack Web Hook ex: https://hooks.slack.com/services/xxxxxxx/xxxxxxxxx/xxxxxxxxxxxxxxxxxxxx
-slackWebHook=''
-#channel to post in slack, include the # in front of the channel ex: #systems
-channel=''
+slackWebHook = SLACKURL + SLACKTOKEN
 
 # Get Server Info
-serverName=socket.gethostbyname_ex(socket.gethostname())[0]
+serverName = socket.gethostbyname_ex(socket.gethostname())[0]
 
 if enableIps:
-        serverIps=socket.gethostbyname_ex(socket.gethostname())[2]
-        if len(serverIps) > 1:
-                externalServerIp=socket.gethostbyname_ex(socket.gethostname())[2][1]
-                internalServerIp=socket.gethostbyname_ex(socket.gethostname())[2][0]
-                # Set the Message that's sent
-                message="%s ip blocked on %s (%s / %s)" % ( sys.argv[1], serverName, externalServerIp, internalServerIp )
-        else:
-                serverIp=socket.gethostbyname_ex(socket.gethostname())[2][0]
-                # Set the Message that's sent
-                message="%s ip blocked on %s (%s)" % ( sys.argv[1], serverName, serverIp )
+    serverIps=socket.gethostbyname_ex(socket.gethostname())[2]
+    if len(serverIps) > 1:
+        externalServerIp = socket.gethostbyname_ex(socket.gethostname())[2][1]
+        internalServerIp = socket.gethostbyname_ex(socket.gethostname())[2][0]
+        # Set the Message that's sent
+        message = '{} ip blocked on {} ({} / {})'.format(sys.argv[1], serverName, externalServerIp, internalServerIp)
+    else:
+        serverIp = socket.gethostbyname_ex(socket.gethostname())[2][0]
+        # Set the Message that's sent
+        message = '{} ip blocked on {} ({})'.format(sys.argv[1], serverName, serverIp)
 else:
-        message="%s ip blocked on %s" % ( sys.argv[1], serverName )
+    message = '{} ip blocked on {}'.format(sys.argv[1], serverName)
         
 
 #if channel is set use the channel defined, otherwise post to the channel the webhook was made for
-if channel != "":
-        data='payload={"text":"' + message + '","channel":"' + channel + '"}'
+if SLACKCHANNEL != "":
+    data = 'payload={"text":"{}","channel":"{}"}'.format(message, SLACKCHANNEL)
 else:
-        data='payload={"text":"' + message + '"}'
+    data = 'payload={"text":"{}"}'.format(message)
 
 if slackWebHook != "":
-        request = urllib2.Request(slackWebHook, data)
-        call = urllib2.urlopen(request)
-        call.close()
+    request = urllib2.Request(slackWebHook, data)
+    call = urllib2.urlopen(request)
+    call.close()
 
 sys.exit(0)
