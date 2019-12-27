5b26f88a8afbb7f808e2652536e30f0b6b12aac9
josev814 <josev814@gmail.com>
2019-12-27 16:40:50 -0500

    testing ipaddress requirements
    adjusting to detect py_version rather than doing a try except clause
    adding a test for the is_valid_ip_address section

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,13 +2,25 @@ from __future__ import print_function, unicode_literals
 
 import unittest
 
-from DenyHosts.util import is_false, is_true, parse_host
+from DenyHosts.util import is_false, is_true, parse_host, is_valid_ip_address
 
 # List of 2-tuples: line to parse, expected host
 HOST_TEST_DATA = [
     ('host', 'host'),
     ('ALL:127.0.0.1', '127.0.0.1'),
     (3, ''),
+]
+
+TEST_IPS = [
+    ('127.0.0.1', False),
+    ('127.0.1.1', False),
+    ('10.5.0.234', False),
+    ('172.16.0.1', False),
+    ('192.168.0.1', False),
+    ('224.2.0.45', False),
+    ('8.8.8.8', True),
+    ('4.4.2.2', True),
+    ('49.88.112.60', True)
 ]
 
 class UtilsTest(unittest.TestCase):
@@ -33,3 +45,7 @@ class UtilsTest(unittest.TestCase):
     def test_parse_host(self):
         for line, expected in HOST_TEST_DATA:
             self.assertEqual(parse_host(line), expected)
+
+    def test_valid_ip(self):
+        for ip, expected in TEST_IPS:
+            self.assertEqual(is_valid_ip_address(ip), expected)
