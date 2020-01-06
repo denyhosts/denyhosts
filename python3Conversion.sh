c14d4623ab5e1a9f3f9fdeb7d0ad70d15dba4c0c
Jose <josev814@gmail.com>
2020-01-06 15:13:43 -0500

    Bug 114 (#115)
    * testing ipaddress requirements
    
    adjusting to detect py_version rather than doing a try except clause
    adding a test for the is_valid_ip_address section
    
    * python version_info for 2.6 isn't a dict
    
    * freebsd py3 conversions
    
    changing to use sh instead of bash since all distros should has sh but
         not bash
    fixing directory check syntax
    using pwd variable instead of ./ which causes syntax issues
    having sed -i use -i.bak which is compatible with bsd and gnu sed
    adding code to remove bak files created from the updated sed -i replacement
         method
    
    * removing deprecated code
    
    removing apply and replacing it with getattr to do the function call

--- python3Conversion.sh
+++ python3Conversion.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-if [[ -d DenyHosts ]]
+if [ -d DenyHosts ]
 then
-    find ./ -type f -exec sed -i 's/bin\/env python/bin\/python3/g' {} \;
-    sed -i 's/ipaddr>=2.1/ip_address/g' requirements.txt
+    py3loc=$(whereis python3 | awk '{ print $2 }')
+    #using sed -i.bak to make it gnu and bsd compatible
+    find `pwd` -type f -exec sed -i.bak "s/\/usr\/bin\/env python/${py3loc}/g" {} \;
+    sed -i.bak 's/ipaddr>=2.1/ip_address/g' requirements.txt
+    find `pwd` -type f -name "*.bak" -exec rm {} \;
 else
     echo "This script must be ran within the root of the denyhosts project\r\n"
 fi
