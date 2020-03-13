4a48f342371a2cebbe276953d131ce8274f254c4
josev814 <josev814@gmail.com>
2020-03-12 22:14:43 -0400

    adding future print function for py2.7 support

--- DenyHosts/allowedhosts.py
+++ DenyHosts/allowedhosts.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from socket import getfqdn, gethostbyname
+from __future__ import print_function
 
 from .constants import ALLOWED_HOSTS, ALLOWED_WARNED_HOSTS
 from .regex import ALLOWED_REGEX
