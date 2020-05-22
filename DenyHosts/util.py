05c2983876647a108824e4f3f2ebcccdd815d2ed
Matthew Blythe <mblythester+git@gmail.com>
2020-05-22 14:22:54 -0600

    Support other methods for sending report e-mail (#93)
    * if SMTP details aren't set, just print the message to stdout.  Assuming the script is run by cron job, then cron should e-mail output to the crontab owner
    
    * add a new config option that selects the method used to send mail.  Supported options are SMTP, SENDMAIL, MAIL, and STDOUT.  I've tested all except for SMTP, but that should be identical to what it used to do
    
    * Don't need default values for SMTP_HOST and SMTP_PORT anymore, since I'm not keying off of them for the e-mail method
    
    Co-authored-by: Jose <josev814@gmail.com>

--- DenyHosts/util.py
+++ DenyHosts/util.py
@@ -3,6 +3,8 @@ import logging.handlers
 from smtplib import SMTP, SMTP_SSL
 from smtplib import SMTPResponseException
 from smtplib import SMTPHeloError
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
 import sys
 from textwrap import dedent
 import time
@@ -116,24 +118,25 @@ def parse_host(line):
 
 def send_email(prefs, report_str):
     recipients = prefs['ADMIN_EMAIL'].split(',')
-
     msg = dedent("""
-        From: {0}
-        To: {1}
-        Subject: {2}
-        Date: {3}
+    From: {0}
+    To: {1}
+    Subject: {2}
+    Date: {3}
 
-        """).lstrip().format(
-            prefs.get('SMTP_FROM'),
-            prefs.get('ADMIN_EMAIL'),
-            prefs.get('SMTP_SUBJECT'),
-            time.strftime(
-                prefs.get('SMTP_DATE_FORMAT')
-            )
+    """).lstrip().format(
+        prefs.get('SMTP_FROM'),
+        prefs.get('ADMIN_EMAIL'),
+        prefs.get('SMTP_SUBJECT'),
+        time.strftime(
+            prefs.get('SMTP_DATE_FORMAT')
+
         )
+    )
 
     msg += report_str
     try:
+        method = prefs.get('EMAIL_METHOD')
         if is_true(prefs.get('SMTP_SSL')):
             smtp = SMTP_SSL()
         else:
@@ -157,33 +160,45 @@ def send_email(prefs, report_str):
                 (code, resp) = smtp.ehlo()
                 if code != 250:
                     raise SMTPResponseException(code, resp)
+            else:
+                # The server does not support esmtp.
+
+                # The Python library SMTP class handles executing HELO/EHLO commands inside
+                # login/sendmail methods when neither helo()/ehlo() methods have been
+                # previously called.  Because we have already called ehlo() above, we must
+                # manually fallback to calling helo() here.
+
+                (code, resp) = smtp.helo()
+                if not (200 <= code <= 299):
+                    raise SMTPHeloError(code, resp)
+
+            username = prefs.get('SMTP_USERNAME')
+            password = prefs.get('SMTP_PASSWORD')
+
+            if username and password:
+                smtp.login(username, password)
+
+            smtp.sendmail(prefs.get('SMTP_FROM'), recipients, msg)
+            debug("sent email to: %s" % prefs.get("ADMIN_EMAIL"))
+        elif method == 'SENDMAIL':
+            msg = MIMEText(report_str)
+            msg["From"] = prefs.get("SMTP_FROM")
+            msg["To"] = prefs.get("ADMIN_EMAIL")
+            msg["Subject"] = prefs.get("SMTP_SUBJECT")
+            p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+            p.communicate(msg.as_string())
+        elif method == 'MAIL':
+            p = Popen(['mail', '-s', prefs.get("SMTP_SUBJECT")] + recipients, stdin=PIPE)
+            p.communicate(report_str)
+        elif method == 'STDOUT':
+            print(report_str)
         else:
-            # The server does not support esmtp.
-
-            # The Python library SMTP class handles executing HELO/EHLO commands inside
-            # login/sendmail methods when neither helo()/ehlo() methods have been
-            # previously called.  Because we have already called ehlo() above, we must
-            # manually fallback to calling helo() here.
-
-            (code, resp) = smtp.helo()
-            if not (200 <= code <= 299):
-                raise SMTPHeloError(code, resp)
-
-        username = prefs.get('SMTP_USERNAME')
-        password = prefs.get('SMTP_PASSWORD')
-
-        if username and password:
-            smtp.login(username, password)
-
-        smtp.sendmail(prefs.get('SMTP_FROM'),
-                      recipients,
-                      msg)
-        debug("sent email to: %s" % prefs.get("ADMIN_EMAIL"))
+            raise Exception("Unknown e-mail method: %s" % method)
     except Exception as e:
         print("Error sending email")
         print(e)
         print("Email message follows:")
-        print(msg)
+        print(report_str)
 
     try:
         smtp.quit()
